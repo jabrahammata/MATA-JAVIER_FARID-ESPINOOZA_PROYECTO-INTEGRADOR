f9b434224bb1665df66b6f95c43905fea5c02e8e
unknown <j.abrahammata@gmail.com>
2024-09-06 23:24:07 -0600

    se agrego controlador y service para Odontologo

---
+++ Clinica-Odontologica/src/main/java/dh/backend/clinicamvc/controller/VistaController.java
@@ -0,0 +1,21 @@
+package dh.backend.clinicamvc.controller;
+
+import dh.backend.clinicamvc.entity.Odontologo;
+import dh.backend.clinicamvc.entity.Paciente;
+import dh.backend.clinicamvc.exception.BadRequestException;
+import dh.backend.clinicamvc.service.IOdontologoService;
+import dh.backend.clinicamvc.service.IPacienteService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
+
+@Controller
+@RequestMapping
+
+public class VistaController {
+    private
+}
