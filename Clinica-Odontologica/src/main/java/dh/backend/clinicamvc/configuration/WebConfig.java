9816b219a718ad5a6176d6a2d2076be479bae54b
unknown <j.abrahammata@gmail.com>
2024-09-06 22:51:40 -0600

    se agregaron las entidades iniciales

---
+++ Clinica-Odontologica/src/main/java/dh/backend/clinicamvc/configuration/WebConfig.java
@@ -0,0 +1,16 @@
+package dh.backend.clinicamvc.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+    public void addCorsMapping(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
